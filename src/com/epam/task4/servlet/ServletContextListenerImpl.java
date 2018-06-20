02cbe557c06ac891d764f73153701d4279a753b9
crazyr <alexey98901@gmail.com>
2018-06-20 22:17:47 +0300

    20.06.2018 22:17

---
+++ src/com/epam/task4/servlet/ServletContextListenerImpl.java
@@ -0,0 +1,33 @@
+package com.epam.task4.servlet;
+
+import com.epam.task4.pool.ConnectionPool;
+import java.sql.Driver;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+
+@WebListener
+public class ServletContextListenerImpl implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        ConnectionPool.INSTANCE.init();
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        ConnectionPool.INSTANCE.closeAll();
+        try {
+            Enumeration<Driver> driverEnumeration = DriverManager.getDrivers();
+
+            while(driverEnumeration.hasMoreElements()) {
+                DriverManager.deregisterDriver(driverEnumeration.nextElement());
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/com/epam/task4/servlet/XMLServlet.java
+++ src/com/epam/task4/servlet/XMLServlet.java
@@ -1,7 +1,6 @@
 package com.epam.task4.servlet;
 
 import com.epam.task4.command.XMLCommandType;
-import com.epam.task4.pool.ConnectionPool;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,18 +12,6 @@ import java.io.IOException;
 @WebServlet(name="XMLParsingServlet",
             urlPatterns = "/XMLServlet")
 public class XMLServlet extends HttpServlet {
-
-    @Override
-    public void init() throws ServletException {
-        super.init();
-        ConnectionPool.INSTANCE.init();
-    }
-
-    @Override
-    public void destroy() {
-        ConnectionPool.INSTANCE.closeAll();
-        super.destroy();
-    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
